c33fe7ee673973adfbac0cbe6fcb400abb80a50a
Paweł Brzozowski <pawel@brzoza.net>
2026-03-05 13:02:51 +0100

    Add openchat:watch script and enhance openchat functionality with embeddings

--- src/week-1/05-openrouter/openchat.ts
+++ src/week-1/05-openrouter/openchat.ts
@@ -11,3 +11,36 @@ const consoleReader = readline.createInterface({
 
 const ask = (question: string) =>
   new Promise<string>((resolve) => consoleReader.question(question, resolve));
+
+async function openchat() {
+  console.log(
+    `
+      🚀 Rozpocznij rozmowę zadając pytania
+      💡 Czatbot pamięta całą historię rozmowy, aż jej nie zakończysz
+      ❌ Aby zakończyć rozmowę, napisz "koniec"
+    `,
+  );
+
+  while (true) {
+    const userInput = await ask("Ty: ");
+    const openrouter = new OpenRouter({
+      apiKey: process.env.OPENROUTER_API_KEY,
+    });
+
+    const embedding = await openrouter.embeddings.generate({
+      requestBody: {
+        model: "nvidia/llama-nemotron-embed-vl-1b-v2:free",
+        input: [
+          {
+            content: [{ type: "text", text: "What is in this image?" }],
+          },
+        ],
+        encodingFormat: "float",
+      },
+    });
+
+    // console.log(embedding.data[0].embedding.slice(0, 5));
+  }
+}
+
+openchat();
